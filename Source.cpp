3ae0c6c71753a85e45933e392977f420668869df
Dominic-Hoe <domhoe2004@gmail.com>
2021-02-07 23:45:43 +0000

    Create IsLoggedIn method

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,20 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+bool IsLoggedIn() {
+	string username, password, un, pw;
+
+	cout << "Enter username: "; cin >> username;
+	cout << "Enter password: "; cin >> password;
+
+	ifstream read("data\\" + username + ".txt");
+	getline(read, un);
+	getline(read, pw);
+	return username == un && password == pw;
+}
 
 int main() {
 	std::cout << "Hello World\r\n";
